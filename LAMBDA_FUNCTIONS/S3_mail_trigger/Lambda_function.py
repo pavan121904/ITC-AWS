a61b15762676e102b169c181d767349978b8fa05
PavanKumar Muri <107158434+pavan121904@users.noreply.github.com>
2023-10-09 12:13:30 +0100

    Update Lambda_function.py

--- LAMBDA_FUNCTIONS/S3_mail_trigger/Lambda_function.py
+++ LAMBDA_FUNCTIONS/S3_mail_trigger/Lambda_function.py
@@ -1,18 +1,26 @@
 import json
 import urllib.parse
+import boto3
 
 def lambda_handler(event, context):
+    # Initialize the SNS client
+    sns_client = boto3.client('sns')
+    
     # Process the S3 event
     for record in event['Records']:
         # Get the S3 bucket name and object key
         s3_bucket = record['s3']['bucket']['name']
         s3_object_key = urllib.parse.unquote_plus(record['s3']['object']['key'])
-
-        # Perform your custom processing here
-        print(f"File uploaded: s3://{s3_bucket}/{s3_object_key}")
-
-        # Add your business logic here
-        # For example, you can process the file, move it, or perform any other action.
+        
+        # Compose the message for the email notification
+        message = f"New file uploaded to S3 bucket: {s3_bucket}\nObject key: {s3_object_key}"
+        
+        # Publish the message to the specified SNS topic
+        sns_client.publish(
+            TopicArn='arn:aws:sns:eu-north-1:866934333672:Default_CloudWatch_Alarms_Topic',
+            Subject='New File Uploaded to S3',
+            Message=message
+        )
 
     return {
         'statusCode': 200,
